fdd3260fd2a83c1a0d83ae77296c58b10f72a1af
Aminder-Khungura <khunguraaminder@gmail.com>
2021-12-27 18:10:02 -0700

    Used CV to detect parking lot boundaries, parking spots and entries.

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
-parkingLot = cv2.imread('Labelled Parking Lot.jpg')
-parkingLot_hsv = cv2.cvtColor(parkingLot, cv2.COLOR_BGR2RGB)
+parkingLot = cv.imread('Labelled Parking Lot.jpg')
+parkingLot_hsv = cv.cvtColor(parkingLot, cv.COLOR_RGB2HSV)
 
-boundary = np.array([63, 72, 204])
-parkingSpot = np.array([237, 28, 36])
-entry_exit = np.array([34, 177, 76])
+lower_green = np.array([50, 100, 100])
+upper_green = np.array([70, 255, 255])
+lower_blue = np.array([0, 100, 100])
+upper_blue = np.array([10, 255, 255])
+lower_red = np.array([110, 100, 100])
+upper_red = np.array([130, 255, 255])
 
-cv2.imshow('Parking Lot', parkingLot)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+boundary_mask = cv.inRange(parkingLot_hsv, lower_blue, upper_blue)
+parkingSpot_mask = cv.inRange(parkingLot_hsv, lower_red, upper_red)
+entry_mask = cv.inRange(parkingLot_hsv, lower_green, upper_green)
+
+boundary_result = cv.bitwise_and(parkingLot, parkingLot, mask=boundary_mask)
+parkingSpot_result = cv.bitwise_and(parkingLot, parkingLot, mask=parkingSpot_mask)
+entry_result = cv.bitwise_and(parkingLot, parkingLot, mask=entry_mask)
+
+blend1 = cv.addWeighted(boundary_result, 1, parkingSpot_result, 1, 0)
+blend2 = cv.addWeighted(blend1, 1, entry_result, 1, 0)
+cv.imshow('Parking Lot', blend2)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
