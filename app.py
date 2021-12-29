b5e1ddb5c42a3b024343289945393d96a81858be
Aminder-Khungura <khunguraaminder@gmail.com>
2021-12-29 16:30:17 -0700

    Reformatted code into OOP

--- app.py
+++ app.py
@@ -5,38 +5,48 @@
 
 import pygame
 
-# Initialize game
-pygame.init()
 
-# Customize screen
-screen = pygame.display.set_mode((1000, 1000))
-pygame.display.set_caption("Snow Plow")
-icon = pygame.image.load('snowplow.png')
-pygame.display.set_icon(icon)
-background_image = pygame.image.load('Edited Parking Lot.jpg').convert()
+class Snowplow:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.snowplow_character = pygame.image.load('snowplow_character.png')
 
-
-def background():
-    screen.blit(background_image, [0, 0])
+    def place_snowplow(self, x, y):
+        self.parent_screen.blit(self.snowplow_character, [x, y])
 
 
-# Snowplow character
-snowplow_character = pygame.image.load('snowplow_character.png')
+class Display:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1000, 1000))
+        pygame.display.set_caption("Snowplow Visualization")
+        self.icon = pygame.image.load('snowplow.png')
+        pygame.display.set_icon(self.icon)
+        self.background_image = pygame.image.load('Edited Parking Lot.jpg').convert()
+        self.screen.blit(self.background_image, [0, 0])
+        self.snowplow = Snowplow(self.screen)
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # Set snowplow character to display on user's click location
+            try:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x_coor, y_coor = pygame.mouse.get_pos()
+                    x_coor -= 30
+                    y_coor -= 30
+                self.screen.blit(self.background_image, [0, 0])
+                self.snowplow.place_snowplow(x_coor, y_coor)
+            except NameError:  # Bypass NameError that occurs if mouse isn't clicked and variables x_coor/y-coor aren't created for snowplow() function
+                pass
+            pygame.display.flip()
 
 
-def snowplow():
-    x = 100
-    y = 100
-    screen.blit(snowplow_character, [x, y])
+if __name__ == '__main__':
+    display = Display()
+    display.run()
 
-
-# Game Loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    # Set background and add snowplow character
-    background()
-    snowplow()
-    pygame.display.flip()
--- process image.py
+++ process image.py
@@ -4,6 +4,7 @@ import numpy as np
 parkingLot = cv.imread('Labelled Parking Lot.jpg')
 parkingLot_hsv = cv.cvtColor(parkingLot, cv.COLOR_RGB2HSV)
 
+# Define color ranges for masks
 lower_green = np.array([50, 100, 100])
 upper_green = np.array([70, 255, 255])
 lower_blue = np.array([0, 100, 100])
@@ -11,14 +12,17 @@ upper_blue = np.array([10, 255, 255])
 lower_red = np.array([110, 100, 100])
 upper_red = np.array([130, 255, 255])
 
+# create masks for boundary, parking spots, and entry
 boundary_mask = cv.inRange(parkingLot_hsv, lower_blue, upper_blue)
 parkingSpot_mask = cv.inRange(parkingLot_hsv, lower_red, upper_red)
 entry_mask = cv.inRange(parkingLot_hsv, lower_green, upper_green)
 
+# Use mask to extract the area of interest
 boundary_result = cv.bitwise_and(parkingLot, parkingLot, mask=boundary_mask)
 parkingSpot_result = cv.bitwise_and(parkingLot, parkingLot, mask=parkingSpot_mask)
 entry_result = cv.bitwise_and(parkingLot, parkingLot, mask=entry_mask)
 
+# Combine results for boundary, parking spots and entry
 blend1 = cv.addWeighted(boundary_result, 1, parkingSpot_result, 1, 0)
 blend2 = cv.addWeighted(blend1, 1, entry_result, 1, 0)
 
@@ -33,6 +37,7 @@ np.savetxt('boundary_coordinates', boundary_coordinates)
 np.savetxt('parkingSpot_coordinates', parkingSpot_coordinates)
 np.savetxt('entry_coordinates', entry_coordinates)
 
+# Save final edited image
 cv.imwrite('Edited Parking Lot.jpg', blend2)
 
 # cv.imshow('Parking Lot', blend2)
