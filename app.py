bbba594f0c2c58d3428c3db08ba22d41efd9fc85
Aminder-Khungura <khunguraaminder@gmail.com>
2022-01-01 23:51:26 -0700

    Added parking lot barriers via OpenCV

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ class Display:
         view = view.transpose([1, 0, 2])
         img_bgr = cv.cvtColor(view, cv.COLOR_RGB2BGR)
         y_boundary, x_boundary = np.where(np.all(img_bgr != [0,   0, 0], axis=2))
-        for i in x_boundary:
-            pygame.draw.circle(self.screen, [0,   0, 255], [x_boundary[i], y_boundary[i]], 1)
+        # for i in x_boundary:
+        #     pygame.draw.circle(self.screen, [0,   0, 255], [x_boundary[i], y_boundary[i]], 1)
 
     def run(self):
         running = True
