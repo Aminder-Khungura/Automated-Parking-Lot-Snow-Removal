9965b12b833ad6bc2914b8fab915c59824de7450
Aminder-Khungura <khunguraaminder@gmail.com>
2022-01-01 23:49:17 -0700

    Added parking lot barriers via OpenCV

--- app.py
+++ app.py
@@ -5,7 +5,6 @@
 
 import pygame
 import cv2 as cv
-import pandas as pd
 import numpy as np
 
 
@@ -22,13 +21,23 @@ class Snowplow:
 class Display:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((987, 964))
+        self.screen = pygame.display.set_mode((1000, 1000))
         pygame.display.set_caption("Snowplow Visualization")
         self.icon = pygame.image.load('snowplow.png')
         pygame.display.set_icon(self.icon)
         self.background_image = pygame.image.load('Edited Parking Lot.jpg').convert()
         self.screen.blit(self.background_image, [0, 0])
         self.snowplow = Snowplow(self.screen)
+
+    def parkinglot_barriers(self):
+        # Convert windowSurface to cv2 Reference: https://stackoverflow.com/questions/19240422/display-cv2-videocapture-image-inside-pygame-surface
+        view = pygame.surfarray.array3d(self.screen)
+        # Convert from (width, height, channel) to (height, width, channel)
+        view = view.transpose([1, 0, 2])
+        img_bgr = cv.cvtColor(view, cv.COLOR_RGB2BGR)
+        y_boundary, x_boundary,  = np.where(np.all(img_bgr != [0,   0, 0], axis=2))
+        for i in x_boundary:
+            pygame.draw.circle(self.screen, [0,   0, 255], [x_boundary[i], y_boundary[i]], 1)
 
     def run(self):
         running = True
@@ -55,6 +64,8 @@ class Display:
                         x_coor += 10
                     self.screen.blit(self.background_image, [0, 0])
                     self.snowplow.place_snowplow(x_coor, y_coor)
+
+                self.parkinglot_barriers()
             pygame.display.flip()
 
 
