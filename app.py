851554bc430f340466a2949d0c7e8aa1c08d6599
Aminder-Khungura <khunguraaminder@gmail.com>
2022-01-05 19:45:35 -0700

    Determined coordinates for barriers

--- app.py
+++ app.py
@@ -29,18 +29,51 @@ class Display:
         self.screen.blit(self.background_image, [0, 0])
         self.snowplow = Snowplow(self.screen)
 
-    def parkinglot_barriers(self):
+    def parkinglot_barriers(self, loop_counter):
         # Convert windowSurface to cv2 Reference: https://stackoverflow.com/questions/19240422/display-cv2-videocapture-image-inside-pygame-surface
         view = pygame.surfarray.array3d(self.screen)
         # Convert from (width, height, channel) to (height, width, channel)
         view = view.transpose([1, 0, 2])
-        img_bgr = cv.cvtColor(view, cv.COLOR_RGB2BGR)
-        y_boundary, x_boundary = np.where(np.all(img_bgr != [0,   0, 0], axis=2))
-        # for i in x_boundary:
-        #     pygame.draw.circle(self.screen, [0,   0, 255], [x_boundary[i], y_boundary[i]], 1)
+        if loop_counter == 1:
+            img_hsv = cv.cvtColor(view, cv.COLOR_BGR2HSV)
+
+            # Define color ranges for masks
+            lower_green = np.array([50, 100, 100])
+            upper_green = np.array([70, 255, 255])
+            lower_blue = np.array([0, 100, 100])
+            upper_blue = np.array([10, 255, 255])
+            lower_red = np.array([110, 100, 100])
+            upper_red = np.array([130, 255, 255])
+
+            # create masks for boundary, parking spots, and entry
+            boundary_mask = cv.inRange(img_hsv, lower_blue, upper_blue)
+            parkingspot_mask = cv.inRange(img_hsv, lower_red, upper_red)
+            entry_mask = cv.inRange(img_hsv, lower_green, upper_green)
+
+            # Use mask to extract the area of interest
+            boundary_result = cv.bitwise_and(img_hsv, img_hsv, mask=boundary_mask)
+            parkingspot_result = cv.bitwise_and(img_hsv, img_hsv, mask=parkingspot_mask)
+            entry_result = cv.bitwise_and(img_hsv, img_hsv, mask=entry_mask)
+
+            kernel_erode = np.ones((2, 2), np.uint8)
+            kernel_close = np.ones((15, 15), np.uint8)
+
+            boundary_erode = cv.erode(boundary_result, np.ones((1, 1), np.uint8))
+            boundary_closing = cv.morphologyEx(boundary_erode, cv.MORPH_CLOSE, kernel_close)
+
+            parkingspot_erode = cv.erode(parkingspot_result, kernel_erode)
+            parkingspot_closing = cv.morphologyEx(parkingspot_erode, cv.MORPH_CLOSE, kernel_close)
+
+            entry_erode = cv.erode(entry_result, kernel_erode)
+            entry_closing = cv.morphologyEx(entry_erode, cv.MORPH_CLOSE, kernel_close)
+
+            y_boundary, x_boundary = np.where(np.all(boundary_closing != [0, 0, 0], axis=2))
+            y_parkingspot, x_parkingspot = np.where(np.all(parkingspot_closing != [0, 0, 0], axis=2))
+            y_entry, x_entry = np.where(np.all(entry_closing != [0, 0, 0], axis=2))
 
     def run(self):
         running = True
+        loop_counter = 1
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -65,7 +98,8 @@ class Display:
                     self.screen.blit(self.background_image, [0, 0])
                     self.snowplow.place_snowplow(x_coor, y_coor)
 
-                self.parkinglot_barriers()
+                self.parkinglot_barriers(loop_counter)
+                loop_counter += 1
             pygame.display.flip()
 
 
--- process image.py
+++ process image.py
@@ -18,31 +18,23 @@ parkingSpot_mask = cv.inRange(parkingLot_hsv, lower_red, upper_red)
 entry_mask = cv.inRange(parkingLot_hsv, lower_green, upper_green)
 
 # Use mask to extract the area of interest
-boundary_result = cv.bitwise_and(parkingLot, parkingLot, mask=boundary_mask)
-parkingSpot_result = cv.bitwise_and(parkingLot, parkingLot, mask=parkingSpot_mask)
-entry_result = cv.bitwise_and(parkingLot, parkingLot, mask=entry_mask)
+boundary_result = cv.bitwise_and(parkingLot_hsv, parkingLot_hsv, mask=boundary_mask)
+parkingSpot_result = cv.bitwise_and(parkingLot_hsv, parkingLot_hsv, mask=parkingSpot_mask)
+entry_result = cv.bitwise_and(parkingLot_hsv, parkingLot_hsv, mask=entry_mask)
 
 # Combine results for boundary, parking spots and entry
 blend1 = cv.addWeighted(boundary_result, 1, parkingSpot_result, 1, 0)
 blend2 = cv.addWeighted(blend1, 1, entry_result, 1, 0)
 
-# Get X and Y coordinates of lines
-# x_boundary, y_boundary = np.where(np.all(boundary_result != [0, 0, 0], axis=2))
-# x_parkingSpot, y_parkingSpot = np.where(np.all(parkingSpot_result != [0, 0, 0], axis=2))
-# x_entry, y_entry = np.where(np.all(entry_result != [0, 0, 0], axis=2))
-# boundary_coordinates = np.column_stack((x_boundary, y_boundary))
-# parkingSpot_coordinates = np.column_stack((x_parkingSpot, y_parkingSpot))
-# entry_coordinates = np.column_stack((x_entry, y_entry))
-
 # Clean up final image before saving
-kernel_erode = np.ones((2, 2), np.uint8)
-kernel_close = np.ones((15, 15), np.uint8)
-erode = cv.erode(blend2, kernel_erode)
-closing = cv.morphologyEx(erode, cv.MORPH_CLOSE, kernel_close)
-
+# kernel_erode = np.ones((2, 2), np.uint8)
+# kernel_close = np.ones((15, 15), np.uint8)
+# erode = cv.erode(blend2, kernel_erode)
+# closing = cv.morphologyEx(erode, cv.MORPH_CLOSE, kernel_close)
 
 # Save final edited image
-cv.imwrite('Edited Parking Lot.jpg', closing)
+pic_RGB = cv.cvtColor(blend2, cv.COLOR_HSV2RGB)
+cv.imwrite('Edited Parking Lot.jpg', pic_RGB)
 
 # cv.imshow('Parking Lot', closing)
 # cv.waitKey(0)
